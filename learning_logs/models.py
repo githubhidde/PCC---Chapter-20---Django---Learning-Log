6d2287d6d1d562569eace50ef034d5e9f73ad455
Hidde Tieleman <h.tieleman87@outlook.com>
2021-04-24 20:43:40 +0200

    Added the possibility to add public posts.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,29 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
+
 class Topic(models.Model):
-    """A topic the user is learning about."""
+    """A topic the user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False)
+
     def __str__(self):
-        """Return a string representation of the model."""
-        return self.text
+        """Return a string representation of the model"""
+        return self.text    
 
 class Entry(models.Model):
-    """Something specific learned about a topic."""
+    """Something specific learned about a topic"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         verbose_name_plural = 'entries'
- 
+
     def __str__(self):
-        """Return a string representation of the model."""
-        # Add an ellipsis ONLY if the entry,
-        # is more than 50 characters long.
-        if self.text > self.text[:50]:
-            return self.text[:50] + "..."
-        elif self.text <= self.text[:50]:
-            return self.text[:50]
+        """Retrun a string representation of the model"""
+        if len(self.text) < 50:
+            return f"{self.text}"
+        else:
+            return f"{self.text[:50]}..."
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,44 +1,52 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
+# Create your views here.
 def index(request):
-    """The home page for Learning Log."""
+    """The home page for Learning Log"""
     return render(request, 'learning_logs/index.html')
 
-def check_topic_owner(owner, request):
-    # A general permissions check --> helper method
-    if owner != request.user:
+#Checks whether logged in user has access to topic
+def check_topic_owner(request, topic):
+    if topic.owner != request.user:
         raise Http404
 
-@login_required
 def topics(request):
-    """Show all topics."""
-    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    """Shows all topics"""
+    # Gets public topics
+    public_topics = Topic.objects.filter(public=True).order_by('date_added')
+    # Gets private topics
+    if request.user.is_authenticated:
+        private_topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+        topics = public_topics | private_topics
+    else: 
+        topics = public_topics
+
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
-@login_required
+
 def topic(request, topic_id):
-    """Show a single topic and all its entries."""
+    """Show a single topic and all its entries"""
     topic = get_object_or_404(Topic, id=topic_id)
-    # Check if the entry actually belongs to the owner
-    check_topic_owner(topic.owner, request)
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 @login_required
 def new_topic(request):
-    """Add a new topic."""
+    """Add a new topic"""
     if request.method != 'POST':
-        # No data submitted; create a blank form.
+        #No data submitted; create a blank form.
         form = TopicForm()
     else:
-        # POST data submitted; process data.
+        #POST data submitted; process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
             new_topic = form.save(commit=False)
@@ -46,51 +54,51 @@ def new_topic(request):
             new_topic.save()
             return redirect('learning_logs:topics')
 
-    # Display a blank or invalid form.
+    #Display a blank or invalid form
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
 @login_required
 def new_entry(request, topic_id):
-    """Add a new entry for a particular topic."""
+    """Add a new entry for a particular topic"""
     topic = get_object_or_404(Topic, id=topic_id)
-    # Check if the entry actually belongs to the owner
-    check_topic_owner(topic.owner, request)
+    check_topic_owner(request, topic)
+
     if request.method != 'POST':
-        # No data submitted; create a blank form.
+        #No data submitted, create a blank form
         form = EntryForm()
     else:
-        # POST data submitted; process data.
+        #POST data submitted; process data
         form = EntryForm(data=request.POST)
         if form.is_valid():
             new_entry = form.save(commit=False)
             new_entry.topic = topic
-            new_entry.save()
+            if new_entry.topic.owner == request.user:
+                new_entry.save()
+            else:
+                return Http404
             return redirect('learning_logs:topic', topic_id=topic_id)
 
-    # Display a blank or invalid form.
+    #Display a blank or invalid form
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
 @login_required
 def edit_entry(request, entry_id):
-    """Edit an existing entry."""
+    """Edit an existing entry"""
     entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
-    # Check if the entry actually belongs to the owner
-    check_topic_owner(topic.owner, request)
-    
+    check_topic_owner(request, topic)
+
     if request.method != 'POST':
-        # Initial request; pre-fill form with the current entry.
+        #Initial request; prefill with the current entry
         form = EntryForm(instance=entry)
     else:
-        # POST data submitted; process data.
+        #POST data submitted; process data
         form = EntryForm(instance=entry, data=request.POST)
         if form.is_valid():
-            new_topic = form.save(commit=False)
-            new_topic.owner = request.user
-            new_topic.save()
+            form.save()
             return redirect('learning_logs:topic', topic_id=topic.id)
 
     context = {'entry': entry, 'topic': topic, 'form': form}
-    return render(request, 'learning_logs/edit_entry.html', context)
+    return render(request, 'learning_logs/edit_entry.html', context) 
